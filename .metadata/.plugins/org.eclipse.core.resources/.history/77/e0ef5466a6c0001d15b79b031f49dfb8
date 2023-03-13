2b05e0d274320a525f08349bae2989cab4d108bc
bin2k3 <127089258+bin2k3@users.noreply.github.com>
2023-03-13 17:30:26 +0700

    Bai

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/77/e0ef5466a6c0001d15b79b031f49dfb8
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dangv
+ *
+ */
+module dangnamkhanh_Project {
+}
---
+++ dangnamkhanh_Project/src/DangNamKhanh/DangNamKhanh_App.java
@@ -0,0 +1,10 @@
+package DangNamKhanh;
+
+public class DangNamKhanh_App 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Dang Nam Khnh hoc Java.");
+	}
+
+}
